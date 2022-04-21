051b7219876eec016f6ce7b6609220f7e0bb18b3
Lynderly Orphan <lorphan@il-k3.msk.21-school.ru>
2022-04-21 22:30:47 +0300

    ex01 finished

--- Lab1/Lab1/ex01/Program.cs
+++ Lab1/Lab1/ex01/Program.cs
@@ -1,33 +1,83 @@
 ﻿using System;
+using System.Linq;
 
 namespace Lab1
 {
     class Program
     {
-        public static byte[] PBlock(in byte[] array, in int[] rule)
+        
+        private static byte[] CreateBytesArray(int size)
         {
-            if (array.Length != rule.Length)
-                throw new Exception("Length of bytes array and rule array must be the same");
+            if (size == 0)
+                throw new Exception("Size must be greater than 0");
             
-            int tmp;
-            byte[] newArray = new byte[array.Length];
-            for (int i = 0; i < rule.Length; ++i)
+            Random rnd = new Random();
+            byte[] bytes = new byte[size];
+            for (int i = 0; i < size; ++i)
             {
-                newArray[rule[i]] = array[i];
+                bytes[i] = (byte)rnd.Next(0, 2);
             }
-            return newArray;
+            return bytes;
         }
 
-        static void Main(string[] args)
+        private static int[] CreateRuleArray(int size)
         {
-            byte[] array = {1, 0, 0, 1};
-            int[] rule = {1, 0, 2, 3};
+            if (size == 0)
+                throw new Exception("Size must be greater than 0");
 
-            var newArray = PBlock(array, rule);
-            foreach (var bit in newArray)
+            Random rnd = new Random();
+            var numbers = Enumerable.Range(0, size).OrderBy(x => rnd.Next()).Take(size);
+            int[] rule = numbers.ToArray();
+            return rule;
+        }
+
+        private static void TestPBlock(int testCount, int size)
+        {
+            for (int i = 0; i < testCount; ++i)
             {
-                Console.WriteLine(bit);
+                var bytes = CreateBytesArray(size);
+                var rule = CreateRuleArray(size);
+                var newBytes = PBlock(bytes, rule);
+
+                Console.Write("Bytes array: ");
+                foreach (var elem in bytes)
+                {
+                    Console.Write(elem);
+                }
+                Console.WriteLine();
+                
+                Console.Write("Rule array: ");
+                foreach (var elem in rule)
+                {
+                    Console.Write(elem);
+                }
+                Console.WriteLine();
+                
+                Console.Write("New bytes array: ");
+                foreach (var elem in newBytes)
+                {
+                    Console.Write(elem);
+                }
+                Console.WriteLine("\n");
             }
+        }
+
+        public static byte[] PBlock(in byte[] bytes, in int[] rule)
+        {
+            if (bytes.Length != rule.Length)
+                throw new Exception("Length of bytes array and rule array must be the same");
+            
+            byte[] newBytes = new byte[bytes.Length];
+            for (int i = 0; i < rule.Length; ++i)
+            {
+                newBytes[rule[i]] = bytes[i];
+            }
+            return newBytes;
+        }
+        
+        public static void Main(string[] args)
+        {
+            TestPBlock(10, 5);
         }
     }
 }
