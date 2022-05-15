7b0161df1b08f867f367bd1c5ecdb746eea34585
newmersedez <trishkk2001@gmail.com>
2022-05-15 17:58:17 +0300

    Skeleton for GF256 operations

---
+++ Crypto/AES/GF256.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AES
+{
+    public static class GF256
+    {
+        public static byte Add(byte firstPolynomial, byte secondPolynomial)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public static byte Multipy(byte firstPolynomial, byte secondPolynomial, byte mod)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public static byte Inverse(byte polynomial)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static byte CheckIrreducibility(byte polynomial)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static byte GetIrreduciblePolynomials(byte Polynomial)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
