e832dbb9922f37e2592f668bda7afecc9a2f84e2
newmersedez <trishkk2001@gmail.com>
2022-05-16 00:39:01 +0300

    GF256: add, multiply and inverse operation finished

--- Crypto/AES/GF256.cs
+++ Crypto/AES/GF256.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace AES
 {
@@ -6,27 +7,109 @@ namespace AES
     {
         public static byte Add(byte firstPolynomial, byte secondPolynomial)
         {
-            throw new NotImplementedException();
+            return (byte)(firstPolynomial ^ secondPolynomial);
         }
         
-        public static byte Multipy(byte firstPolynomial, byte secondPolynomial, byte mod)
+        public static byte Multipy(byte firstPolynomial, byte secondPolynomial, UInt16 modulo)
         {
-            throw new NotImplementedException();
+            byte result = 0;
+            while (firstPolynomial != 0 && secondPolynomial != 0)
+            {
+                if ((secondPolynomial & 1) != 0)
+                    result ^= firstPolynomial;
+                if ((firstPolynomial & 0x80) != 0)
+                    firstPolynomial = (byte)((firstPolynomial << 1) ^ modulo);
+                else
+                    firstPolynomial <<= 1;
+                secondPolynomial >>= 1;
+            }
+            return result;
         }
         
-        public static byte Inverse(byte polynomial)
+        public static byte Inverse(byte polynomial, UInt16 modulo)
         {
-            throw new NotImplementedException();
+            UInt16 degree = 254;
+            byte result = 1;
+            UInt16 bitesForMask = degree;
+            while (bitesForMask > 0)
+            {
+                if ((bitesForMask & 0b01) == 1)
+                {
+                    result = Modulo(Multipy(result, polynomial, modulo), modulo);
+                }
+                bitesForMask >>= 1;
+                polynomial = Multipy(polynomial, polynomial, modulo);
+            }
+            return result;
         }
 
-        public static byte CheckIrreducibility(byte polynomial)
+        public static bool CheckIrreducibility(byte polynomial)
         {
-            throw new NotImplementedException();
+            UInt16 num = 0;
+            UInt16 one = 1;
+            
+            for (UInt16 i = 0; i < 14; i++)
+            {
+                if((one & polynomial) != 0)
+                    num = i;
+                one <<= 1;
+            }
+
+            return num == 0;
         }
 
         public static byte GetIrreduciblePolynomials(byte Polynomial)
         {
             throw new NotImplementedException();
+        }
+        
+        private static UInt16 HigherBits(UInt16 polynomial)
+        {
+            if (polynomial == 0)
+                return 0;
+            UInt16 count = 1;
+            while (polynomial != 1)
+            {
+                polynomial >>= 1;
+                count <<= 1;
+            }
+
+            return count;
+        }
+
+        private static UInt16 MultiplyHelper(UInt16 left, UInt16 right)
+        {
+            if (right == 0)
+                return 0;
+            while (right != 1)
+            {
+                left <<= 1;
+                right >>= 1;
+            }
+            return left;
+        }
+        
+        private static byte Modulo(UInt16 num, UInt16 modulo)
+        {
+            UInt16 lDeg = HigherBits(num);
+            UInt16 baseDeg = HigherBits(modulo);
+
+            while (lDeg >= baseDeg)
+            {
+                UInt16 tmpLeft = lDeg;
+                UInt16 tmpRight = baseDeg;
+                while (tmpRight != 1)
+                {
+                    tmpLeft >>= 1;
+                    tmpRight >>= 1;
+                }
+
+                UInt16 tmpMulResult = MultiplyHelper(modulo, tmpLeft);
+                num ^= tmpMulResult;
+                lDeg = HigherBits(num);
+            }
+            
+            return (byte)num;
         }
     }
 }
--- Crypto/AES/Program.cs
+++ Crypto/AES/Program.cs
@@ -6,7 +6,29 @@ namespace AES
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            {
+                byte a = 3;
+                byte b = 5;
+                Console.WriteLine(GF256.Add(a, b));
+            }
+
+            {
+                const UInt16 modulo = 0b100011011;
+                byte a = 15;
+                byte b = 7;
+                Console.WriteLine(GF256.Multipy(a, b, modulo));
+            }
+
+            {
+                const UInt16 modulo = 0b100011011;
+                byte a = 123;
+                Console.WriteLine(GF256.Inverse(a, modulo));
+            }
+
+            {
+                byte a = 11;
+                Console.WriteLine(GF256.CheckIrreducibility(a));
+            }
         }
     }
 }
